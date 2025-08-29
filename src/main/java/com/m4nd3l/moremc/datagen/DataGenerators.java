750a7f8965b295e46f8abe20a92dc73609f5ed5a
M4nd3l <mandel123e@gmail.com>
2025-08-29 20:48:48 +0200

    Fixed Data Gen Recipes

--- src/main/java/com/m4nd3l/moremc/datagen/DataGenerators.java
+++ src/main/java/com/m4nd3l/moremc/datagen/DataGenerators.java
@@ -28,6 +28,8 @@ public class DataGenerators {
         generator.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(),
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
 
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
+
         BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
         generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
